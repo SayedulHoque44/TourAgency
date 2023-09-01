122b82b7d3955f80b40606ae70ca082a6723609e
sayedulhoque44 <sayedulhoque3544@gmail.com>
2023-09-01 11:35:45 +0600

    First structure

---
+++ .eslintrc-config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'no-unused-vars': 'error',
+    'no-undef': 'error',
+    'no-unused-expressions': 'error',
+    'no-unreachable': 'error',
+    'prettier/prettier': 'error',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/consistent-type-definitions': ['error', 'type'],
+  },
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  globals: {
+    process: 'readonly',
+  },
+};
